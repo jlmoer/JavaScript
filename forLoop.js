64d481be2ba9c2f5da926688cfddfc8664e41de1
jlmoer <jlmoerdijk@gmail.com>
2022-01-03 16:27:08 +0100

    Suite des exercices de révision

--- forLoop.js
+++ forLoop.js
@@ -26,3 +26,17 @@ for (let i = 1; i <= 20; i++) {
     }
 }
 console.log(sum);
+
+// 04 - Des Boîtes
+
+//  Le console.log à l'intérieur de la boucle affiche 10 dans la console
+//  console.log(box1) affiche 18
+//  console.log(box2) affiche également 18 dans la console puisque box1 et box2 sont égaux
+//  console.log(i) affiche 13 dans la console
+
+// 05 - Des setiob
+
+// Le console.log(box1) à l'intérieur de la boucle affiche 12 dans la console
+// Le console.log(box1) en dehors de la boucle affiche 42 dans la console
+// Le console.log(i) affiche 0 dans la console
+
