e0ada4ef07cba708de7c81d39dad3b60a5709bdf
jlmoer <jlmoerdijk@gmail.com>
2022-01-04 14:57:08 +0100

    Suite whileLoop

--- whileLoop.js
+++ whileLoop.js
@@ -1,13 +1,34 @@
 // 01 - Comptons
 
-var x = 50;
-while (x !== 101) {
-  if (x % 2 === 0)
-    console.log(x);
-  x++;
-}
+// var x = 50;
+// while (x !== 201) {
+//   if (x % 2 === 0)
+//     console.log(x);
+//   x++;
+// }
 
  // 02 - Try Again
 
- let dice = null;
- let count = 0;
+//  let dice = null;
+//  let count = 0;
+//  while (dice !== 6) {
+//     dice = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+//     count++;
+//  }
+//  console.log(count);
+
+// 03 - Course
+
+let usainBolt = 0;
+let tysonGay = 0;
+while (usainBolt <= 100 && tysonGay <= 100) {
+    let random1 = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    let random2 = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    usainBolt += random1;
+    tysonGay += random2;
+} 
+if (usainBolt >= 100) {
+    console.log(`Usain Bolt wins with ${usainBolt}`);
+} else {
+    console.log(`Tyson Gay wins with ${tysonGay}`);
+}
