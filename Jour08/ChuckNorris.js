acf67aefcda09037997845711bec36b3b1540808
jlmoer <jlmoerdijk@gmail.com>
2022-01-11 16:01:02 +0100

    Exercice Chuck Norris

---
+++ Jour08/ChuckNorris.js
@@ -0,0 +1,13 @@
+// 02 - Chuck Norris
+
+const axios = require('axios');
+
+function getFact() {
+    axios.get('https://api.chucknorris.io/jokes/random').then(function (res){
+        // console.log(res.data);
+        console.log(res.data.value); 
+    })
+}
+
+getFact()
+
